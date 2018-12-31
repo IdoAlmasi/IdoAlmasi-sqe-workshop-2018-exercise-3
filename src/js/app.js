657142fdbfd1dc70c0200c4bee45fb822bcf9095
IdoAlmasi <almasii@post.bgu.ac.il>
2018-12-31 18:36:47 +0200

    Added steps 1-3: created merge points omitted exceptions in esgraph code

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,37 @@
 import $ from 'jquery';
-import {parseCode} from './code-analyzer';
+import {createGraph, parseCode} from './code-analyzer';
+import Viz from 'viz.js';
+import {Module , render} from 'viz.js/full.render.js';
+
+function renderDot(dot){
+    let graphElement = document.getElementById('graph');
+    let viz = new Viz({ Module, render});
+    viz.renderSVGElement(dot)
+        .then(function(element) {
+            graphElement.innerHTML = '';
+            graphElement.append(element);
+        });
+}
 
 $(document).ready(function () {
     $('#codeSubmissionButton').click(() => {
         let codeToParse = $('#codePlaceholder').val();
-        let parsedCode = parseCode(codeToParse);
-        $('#parsedCode').val(JSON.stringify(parsedCode, null, 2));
+        let cfg = createGraph(codeToParse);
+        $('#cfg').val('digraph G {' + cfg + '}');
+        renderDot('digraph G {' + cfg + '}');
+
+        /* let parsedCode = parseCode(codeToParse);
+         let substitutedJson = symbolicSubstitution(parsedCode);
+         let tmp = genCode(substitutedJson);
+         let withUpdatedLocs = parseCode(tmp);
+         let generatedCode = genCode(withUpdatedLocs);
+         $('#parsedCode').val(generatedCode);
+         let functionCode = getFunctionCode(withUpdatedLocs);
+         let inputVector = extractInputVector(parseCode($('#inputVectorPlaceholder').val()));
+         let evalResults = evalCode(functionCode, inputVector);
+         paintStatements(generatedCode, evalResults);
+         //$('#inputVectorPlaceholder').val(JSON.stringify(evalResults));
+         */
+
     });
 });
--- src/js/code-analyzer.js
+++ src/js/code-analyzer.js
@@ -1,7 +1,60 @@
 import * as esprima from 'esprima';
+import * as esgraph from 'esgraph';
 
 const parseCode = (codeToParse) => {
     return esprima.parseScript(codeToParse);
 };
 
-export {parseCode};
+function createGraph(codeToParse) {
+    let parsedCode = parseCode(codeToParse);
+    let flowStruct = esgraph(parsedCode.body[0].body , { omitExceptions: true });
+    //createMergePoints(flowStruct);
+    createMergePoints(flowStruct);
+    let dotStruct = esgraph.dot(flowStruct, {counter: 0, source: parsedCode.body[0].body});
+    return dotStruct;
+
+}
+
+function createMergePoints(flowStruct){ //maybe need to update the next and prev fields of the nodes
+    flowStruct[2].map(node =>{
+        if(node.prev.length>1){
+            let mergePoint = {astNode: undefined , next:[node], normal: node , parent: undefined , prev:node.prev , type:determineType(node)};
+            node.prev.map(x=> {
+                if(x.normal)
+                    x.normal = mergePoint;
+                else if(nodeInTrueValue(x , node))
+                    x.true = mergePoint;
+                else if(nodeInFalseValue(x , node))
+                    x.false = mergePoint;
+                updateNextField(x , node , mergePoint);
+
+            });
+            node.prev = [mergePoint];
+            flowStruct[2].push(mergePoint);
+        }
+    });
+}
+
+function determineType(node) {
+    if(node.parent.type==='WhileStatement')
+        return 'While';
+    else
+        return 'If';
+}
+
+function updateNextField(x , node , mergePoint) {
+    for(let i=0; i<x.next.length; i++){
+        if(x.next[i]===node)
+            x.next[i] = mergePoint;
+    }
+}
+
+function nodeInTrueValue(x , node) {
+    return x.true && x.true === node;
+}
+
+function nodeInFalseValue(x , node) {
+    return x.false && x.false === node;
+}
+
+export {parseCode , createGraph};
